7ebd93edde5707a7df1299de80c89d7bfa46ad27
B.P aka Mince <mindsmend@gmail.com>
2026-03-06 22:34:44 +0000

    Create Page “events”

---
+++ src/pages/events.js
@@ -0,0 +1,16 @@
+---
+slug: events
+title: Events
+description: "Planned Events can be listed here "
+---
+F﻿C of FC
+
+Release 3 games this year.
+
+1. Retro-Space-Game \[Multiplayer]
+2. Cart Racer \[Multiplayer]
+3. Ultimate-Crusher-chain-reactor
+
+Releases 2027
+
+1. Sword and Shield Simulator ( Pans Valhalla Masters ) \[ Multiplayer]
